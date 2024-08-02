e188a84b94d20103b8f41443d39c6e565249f7a5
yusuke-shigeta <dnw.webx@gmail.com>
2024-08-03 01:51:14 +0900

    firstviewの出しわけ簡略化

--- wordpress/wp-content/themes/yk-original/functions.php
+++ wordpress/wp-content/themes/yk-original/functions.php
@@ -65,51 +65,41 @@ add_action('wp_enqueue_scripts', 'enqueue_jquery');
  */
 function get_firstview_data()
 {
-  $pages = ['top', 'inquiry', 'achievements'];
-  $data = [];
-
-  foreach ($pages as $page) {
-    $data[$page] = [
-      'image' => "firstview-page-{$page}.jpg",
-      'heading' => [
-        'top' => 'トップページ',
-        'inquiry' => 'お問い合わせ',
-        'achievements' => '施工実績',
-      ][$page],
+  $pages = [
+    'top' => [
+      'heading' => 'トップページ',
       'text' => [
-        'top' => [
-          'トップページの1行目',
-          'トップページの2行目',
-          'トップページの3行目',
-        ],
-        'inquiry' => [
-          'お問い合わせの1行目',
-          'お問い合わせの2行目',
-          'お問い合わせの3行目',
-        ],
-        'achievements' => [
-          '施工実績の1行目',
-          '施工実績の2行目',
-          '施工実績の3行目',
-        ],
-      ][$page],
-    ];
-  }
+        'トップページの1行目',
+        'トップページの2行目',
+        'トップページの3行目'
+      ],
+    ],
+    'inquiry' => [
+      'heading' => 'お問い合わせ',
+      'text' => [
+        'お問い合わせの1行目',
+        'お問い合わせの2行目',
+        'お問い合わせの3行目',
+      ],
+    ],
+    'achievements' => [
+      'heading' => '施工実績',
+      'text' => [
+        '施工実績の1行目',
+        '施工実績の2行目',
+        '施工実績の3行目',
+      ],
+    ],
+  ];
 
-  $current_page = '';
-  foreach ($pages as $page) {
+  foreach ($pages as $page => $content) {
     if (is_page($page)) {
-      $current_page = $page;
-      break;
+      return [
+        'background_image' => get_template_directory_uri() . "/assets/img/firstview-page-{$page}.jpg",
+        'heading' => $content['heading'],
+        'text' => implode("<br>", $content['text']),
+      ];
     }
-  }
-
-  if (isset($data[$current_page])) {
-    return [
-      'background_image' => get_template_directory_uri() . '/assets/img/' . $data[$current_page]['image'],
-      'heading' => $data[$current_page]['heading'],
-      'text' => implode("<br>", $data[$current_page]['text']),
-    ];
   }
 
   return null;
