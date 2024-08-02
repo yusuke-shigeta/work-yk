4ef6c78537b8fb407e380dfbbe6604e0a57523dd
yusuke-shigeta <dnw.webx@gmail.com>
2024-08-03 01:54:48 +0900

    firstview_dataの使い方変更

--- wordpress/wp-content/themes/yk-original/element/firstview.php
+++ wordpress/wp-content/themes/yk-original/element/firstview.php
@@ -2,17 +2,14 @@
 // firstview
 
 $firstview_data = get_firstview_data();
-$firstview_bg_image = $firstview_data['background_image'];
-$firstview_heading = $firstview_data['heading'];
-$firstview_text = $firstview_data['text'];
 ?>
 
-<section style="background-image: url(<?php echo $firstview_bg_image; ?>)" class="sec">
+<section style="background-image: url(<?php echo $firstview_data['background_image']; ?>)" class="sec">
   <div class="sec-inner">
-    <h1><?php echo $firstview_heading; ?></h1>
-    <?php if ($firstview_text) : ?>
+    <h1><?php echo $firstview_data['heading'];; ?></h1>
+    <?php if ($firstview_data['text']) : ?>
       <p>
-        <?php echo $firstview_text; ?>
+        <?php echo $firstview_data['text']; ?>
       </p>
     <?php endif; ?>
   </div>
