a6625bca5ef75e63ecae1fc7df34f654d5f2ec77
yusuke-shigeta <dnw.webx@gmail.com>
2024-08-22 22:39:29 +0900

    formのフロント部分作成

--- wordpress/wp-content/themes/yk-original/page-contact.php
+++ wordpress/wp-content/themes/yk-original/page-contact.php
@@ -8,6 +8,39 @@
   @include(get_template_directory() . '/element/Firstview.php');
   ?>
 
+  <form action="" method="post">
+    <label for="name">名前:</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="email">メールアドレス:</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="message">メッセージ:</label>
+    <textarea id="message" name="message" required></textarea>
+
+    <input type="submit" name="submit" value="送信">
+  </form>
+
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = sanitize_text_field($_POST['name']);
+    $email = sanitize_email($_POST['email']);
+    $message = sanitize_textarea_field($_POST['message']);
+
+    $to = "dnw.webx@gmail.com"; // 送信先のメールアドレスに変更してください
+    $subject = "お問い合わせフォームからのメッセージ";
+    $headers = "From: $email";
+
+    $body = "名前: $name\nメールアドレス: $email\nメッセージ:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+      echo '<p>メッセージが送信されました。</p>';
+    } else {
+      echo '<p>メッセージの送信に失敗しました。</p>';
+    }
+  }
+  ?>
+
 </main>
 
 <?php get_footer(); ?>
