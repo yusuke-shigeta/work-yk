a4c1d6eee4dddc49e51c94893a18118ea3af74b2
yusuke-shigeta <dnw.webx@gmail.com>
2024-08-26 21:16:53 +0900

    サーバーディレクトリをクリア

--- wordpress/wp-content/themes/yk-original/page-contact.php
+++ wordpress/wp-content/themes/yk-original/page-contact.php
@@ -33,8 +33,6 @@
     $to = "dnw.webx@gmail.com"; // 送信先のメールアドレスに変更してください
     $subject = "お問い合わせフォームからのメッセージ";
     $headers = "From: $email";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
 
     $body = "名前: $name\nメールアドレス: $email\nメッセージ:\n$message";
 
@@ -44,8 +42,6 @@
       $user_subject = "お問い合わせありがとうございます";
       $user_body = "お問い合わせありがとうございます。\n以下の内容でお問い合わせを受け付けました。\n\n" . $body;
       $user_headers = "From: no-reply@yourdomain.com"; // 送信元のメールアドレスに変更してください
-      $user_headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-      $user_headers .= "MIME-Version: 1.0\r\n";
 
       wp_mail($email, $user_subject, $user_body, $user_headers);
     } else {
@@ -60,5 +56,6 @@
   ?>
 
 </main>
+<?php phpinfo(); ?>
 
 <?php get_footer(); ?>
