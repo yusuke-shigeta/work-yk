c2c080b49dd30463598d53662d6e2a1dc64c988b
yusuke-shigeta <dnw.webx@gmail.com>
2024-08-26 20:04:18 +0900

    elementディレクトリをローカルでもサーバーでも入れる

--- wordpress/wp-content/themes/yk-original/element/Title-head2.php
+++ wordpress/wp-content/themes/yk-original/element/Title-head2.php
@@ -2,3 +2,4 @@
   <span class="title-head2-en"><img class="title-head2-en-img" src="<?php echo get_template_directory_uri() ?>/assets/img/<?php echo $title_head2_img_en; ?>" alt="<?php echo $title_head2_ja; ?>" width="<?php echo $title_head2_img_en_width; ?>" height="<?php echo $title_head2_img_en_height; ?>"></span>
   <span class="title-head2-ja"><?php echo $title_head2_ja; ?></span>
 </h2>
+<!-- elementディレクトリなくなったんだけど。 -->
