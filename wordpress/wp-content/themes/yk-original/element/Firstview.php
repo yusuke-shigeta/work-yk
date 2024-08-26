70c53ef60f9d319266dd79933829954fd49bbbb3
yusuke-shigeta <dnw.webx@gmail.com>
2024-08-26 21:24:23 +0900

    大文字小文字を区別させる

---
+++ wordpress/wp-content/themes/yk-original/element/Firstview.php
@@ -0,0 +1,5 @@
+<section class="sec firstview" style="background-image: url('<?php echo get_template_directory_uri() . '/assets/img/' . $fistview_bg; ?>')">
+  <div class="sec-inner">
+    <h1 class="firstview-title"><?php echo $firstview_title ?></h1>
+  </div>
+</section>
