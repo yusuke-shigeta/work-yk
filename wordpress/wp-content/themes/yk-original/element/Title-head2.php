a1504a33e7da3dad04dcc5243d91eb1e35465f1b
yusuke-shigeta <dnw.webx@gmail.com>
2024-08-28 00:03:08 +0900

    リファクタリング

--- wordpress/wp-content/themes/yk-original/element/Title-head2.php
+++ wordpress/wp-content/themes/yk-original/element/Title-head2.php
@@ -1,8 +1,9 @@
 <h2 class="title title-head2">
   <span class="title-head2-ja <?php if (isset($title_head2_color)) echo 'text-color-' . $title_head2_color; ?>">
     <?php echo $title_head2_ja; ?>
-    <span class="title-head2-ja-parts <?php if (isset($title_head2_color)) echo 'title-head2-ja-parts-color-' . $title_head2_color; ?>"></span>
-    <span class="title-head2-ja-parts <?php if (isset($title_head2_color)) echo 'title-head2-ja-parts-color-' . $title_head2_color; ?>"></span>
+    <?php for ($i = 0; $i < 2; $i++): ?>
+      <span class="title-head2-ja-parts <?php if (isset($title_head2_color)) echo 'title-head2-ja-parts-color-' . $title_head2_color; ?>"></span>
+    <?php endfor; ?>
   </span>
   <span class="title-head2-en">
     <img class="title-head2-en-img" src="<?php echo get_template_directory_uri() ?>/assets/img/<?php echo $title_head2_img_en; ?>" alt="<?php echo $title_head2_ja; ?>" width="<?php echo $title_head2_img_en_width; ?>" height="<?php echo $title_head2_img_en_height; ?>">
