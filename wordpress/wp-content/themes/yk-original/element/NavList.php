036b2aeddcd052d7093a5a5ff9255da9caaefd25
yusuke-shigeta <dnw.webx@gmail.com>
2024-08-30 00:06:09 +0900

    footer

--- wordpress/wp-content/themes/yk-original/element/NavList.php
+++ wordpress/wp-content/themes/yk-original/element/NavList.php
@@ -1,8 +1,11 @@
 <ul class="navList <?php echo $navList_unique_class ?>">
   <?php foreach ($menu_items as $key => $item) : ?>
     <li class="navItem">
-      <a class="navLink footer-navLink" href="<?php echo esc_url(home_url($item['link'])); ?>/">
+      <a class="navLink" href="<?php echo esc_url(home_url($item['link'])); ?>/">
         <?php echo esc_html($item['text']); ?>
+        <?php if ($item['text_en']) : ?>
+          <span class="navLink-text-en"><?php echo esc_html($item['text_en']); ?></span>
+        <?php endif; ?>
       </a>
     </li>
   <?php endforeach; ?>
--- wordpress/wp-content/themes/yk-original/footer.php
+++ wordpress/wp-content/themes/yk-original/footer.php
@@ -1,42 +1,47 @@
 <footer class="footer">
-  <div class="footer-inner ">
-    <a class="logo footer-logo" href="<?php echo home_url() ?>">
-      <img src="<?php echo get_template_directory_uri() ?>/assets/img/header-logo.png" alt="Logo" width="61" height="24">
-    </a>
-    <nav class="nav">
+  <div class="footer-inner">
 
+    <div class="footer-info">
+      <a class="logo footer-info-logo" href="<?php echo home_url() ?>">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/header-logo.png" alt="Logo" width="61" height="24">
+      </a>
+      <p class="footer-info-text">株式会社YK</p>
+      <p class="footer-info-text">〒000-0000</p>
+      <p class="footer-info-text">住所テキストテキストテキストテキストテキスト</p>
+      <a class="footer-info-sns" href="">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/icon-instagram-gray.png" alt="Instagram">
+      </a>
+    </div>
+
+    <nav class="nav">
       <?php
+      $navList_unique_class = "footer-navList";
       $menu_items = [
         "top" => [
           "link" => '',
-          "text" => "TOP",
+          "text" => 'トップ',
+          "text_en" => 'Top'
         ],
         "works" => [
           "link" => 'works',
           "text" => "施工実績",
+          "text_en" => "Works",
         ],
         "company" => [
           "link" => '',
           "text" => "会社概要",
+          "text_en" => "Company",
         ],
       ];
+
       @include(get_template_directory() . '/element/NavList.php')
       ?>
-
     </nav>
-    <div class="footer-info">
-      <p class="footer-info-text">株式会社YK</p>
-      <p class="footer-info-text">〒000-0000</p>
-      <p class="footer-info-text">住所テキストテキストテキストテキストテキスト</p>
-    </div>
-    <ul class="snsList">
-      <li class="snsLink snsLink-sns">
-        <a href=""><img src="<?php echo get_template_directory_uri() ?>/assets/img/icon-instagram-gray.png" alt="Instagram"></a>
-      </li>
-    </ul>
+
     <p class="footer-copyright">
       <?php printf(esc_html__('© %1$s %2$s', 'yk-original'), date('Y'), get_bloginfo('name')); ?>
     </p>
+
   </div>
 </footer>
 
