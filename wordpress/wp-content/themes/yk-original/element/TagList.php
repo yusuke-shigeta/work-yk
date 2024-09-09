4176608db16459f8439c8ad948ca68041b06c4f2
yusuke-shigeta <dnw.webx@gmail.com>
2024-09-09 23:52:32 +0900

    archive-work

--- wordpress/wp-content/themes/yk-original/element/TagList.php
+++ wordpress/wp-content/themes/yk-original/element/TagList.php
@@ -1,7 +1,7 @@
 <ul class="tagList">
   <?php foreach ($work_tags as $tag) : ?>
     <li class="tagItem">
-      <a href="<?php echo get_term_link($tag->term_id); ?>" class="btn-color-gray tagLink">
+      <a href="<?php echo get_term_link($tag->term_id); ?>" class="btn-color-gray tagBtn">
         <?php echo $tag->name; ?>
         (<?php echo $tag->count; ?>)
       </a>
