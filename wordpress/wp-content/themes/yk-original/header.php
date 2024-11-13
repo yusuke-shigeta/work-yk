21ffa04e56ce15d619d825c61c37f5bd65813fb8
yusuke-shigeta <dnw.webx@gmail.com>
2024-11-13 20:03:06 +0900

    update, ズーム設定

--- wordpress/wp-content/themes/yk-original/header.php
+++ wordpress/wp-content/themes/yk-original/header.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <link rel="profile" href="https://gmpg.org/xfn/11">
   <?php wp_head(); ?>
 </head>
